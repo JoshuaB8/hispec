205e02226f3b61864b663b0f77a10414a3f0eafc
Josh@Hi-Spec <42446797+JoshuaB8@users.noreply.github.com>
2021-11-09 17:38:30 +0000

    hispec
    mass file upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+    setTimeout(function(){
+        var Alert = new CustomAlert();
+        2
+        3
+        function CustomAlert(){
+        4
+          this.render = function(){
+        5
+              //Show Modal
+        6
+              let popUpBox = document.getElementById('popUpBox');
+        7
+              popUpBox.style.display = "block";
+        8
+              //Close Modal
+        9
+              document.getElementById('closeModal').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+        10
+          }
+        11
+          
+        12
+        this.ok = function(){
+        13
+          document.getElementById('popUpBox').style.display = "none";
+        14
+          document.getElementById('popUpOverlay').style.display = "none";
+        15
+        }
+        16
+        }
+    },5000); // 5000 to load it after 5 seconds from page load
+ });
